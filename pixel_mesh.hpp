2ea15b3bf4e57bd756af9bd6780bbb2201979446
sweetkev <kevinsweet00@gmail.com>
2025-11-20 15:49:15 -0800

    added comments and removed unused functions

--- pixel_mesh.hpp
+++ pixel_mesh.hpp
@@ -32,25 +32,20 @@ public:
 
    PixelMesh(std::string pgm_file) : PixelMesh(PixelImage(pgm_file)) { }
 
-   // PixelMesh(std::string pgm_file);
-   // PixelMesh(Mesh &mesh_, const std::unordered_map<Coord, int> &coord_to_vertex_, int width_, int height_);
+   /** Returns coarsened mesh obtained from current mesh */
+   PixelMesh PixelMesh::CoarsenMesh() { return PixelMesh(image.Coarsen()); }
 
-   /** Creates coarse mesh from current mesh */
-   PixelMesh CoarsenMesh();
 
-   // /** Creates prolongation map from current mesh to finer mesh */
-   // SparseMatrix CreateProlongation(PixelMesh fine_mesh);
-
-   // /** Returns associated mesh */
+   /** Returns associated mesh */
    Mesh &GetMesh() { return mesh; }
 
+   /** Returns associated mesh as const */
    const Mesh &GetMesh() const { return mesh; }
 
-   // /** Returns associated coordinate pair to vertex map */
-   // const std::unordered_map<Coord, int> &GetVertexMap() { return coord_to_vertex; }
-
+   /** Returns the coordinates associated with element i */
    Coord GetElementCoord(int i) const { return element_to_coord.at(i); }
 
+   /** Returns the element index with coordinates coord */
    int GetElementIndex(Coord coord) const
    {
       auto it = coord_to_element.find(coord);
@@ -74,30 +69,6 @@ private:
 
    int width = -1;
    int height = -1;
-
-   // /** Makes mesh given PixelImage, with elements given by filled-in pixels */
-   // std::tuple<std::unordered_map<Coord, int>, Mesh> MakeMesh(const PixelImage &image);
-
-   // /** Returns true when a pixel adjacent to vertex is filled in. Else returns false. */
-   // bool AdjacentPixelFilled(int i, int j, const PixelImage &image);
-
-   // /** Adds vertices to mesh at integer indicies, so pixels will be 1x1. Returns map taking coordinates to vertex number. */
-   // void AddVertices(const PixelImage &image, Mesh &mesh, int m, int n);
-
-   // /** Adds quads for filled pixels */
-   // void AddQuads(const PixelImage &image, Mesh &mesh, int m, int n);
-
-   // /** Returns true if any adjacent coarse pixel should be filled in. Else returns false */
-   // bool PixelNearby(int x, int y, const std::unordered_map<Coord, int> &coord_to_fine_vertex);
-
-   // /** Returns unorderd map that takes coordinates to coarse vertex, and adds vertices to coarse mesh */
-   // std::unordered_map<Coord, int> AddCoarseVertices(Mesh &coarse_mesh, int m, int n, const std::unordered_map<Coord, int> &coord_to_fine_vertex);
-
-   // /** Adds quads to coarse mesh for filled pixels */
-   // void AddCoarseQuads(Mesh &coarse_mesh, int m, int n, const std::unordered_map<Coord, int> &coord_to_fine_vertex);
-
-   // /** Adds the influence of a vertex in the fine mesh to the prolongation matrix P */
-   // void AddVertexInfluence(int x, int y, SparseMatrix &P, const std::unordered_map<Coord, int> &fine_coord_to_vertex);
 };
 
 SparseMatrix CreatePixelProlongation(const PixelMesh &coarse_mesh,
