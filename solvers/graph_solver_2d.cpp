d5d4f36721b11a1839fbdd734c51ce881fec5969
sweetkev <kevinsweet00@gmail.com>
2025-12-26 11:35:41 -0800

    basic scaffolding for graph solver

--- solvers/graph_solver_2d.cpp
+++ solvers/graph_solver_2d.cpp
@@ -1,13 +1,52 @@
 #include "mfem.hpp"
 #include "ppm.hpp"
+#include "pixel_mesh.hpp"
 
 using namespace mfem;
 using namespace std;
 
-//generate fine mesh from pgm file
+int main(int argc, char *argv[])
+{
 
-//create finite element space
+    //parse options
+    string pgm_file = "../pgm_files/australia.pgm";
+    int nlevels = -1;
+    int order = 1;
 
-//create multigrid operator
+    OptionsParser args(argc, argv);
+    args.AddOption(&pgm_file, "-f", "--file", "pgm file to use");
+    args.AddOption(&nlevels, "-nl", "--nlevels", "number of multigrid levels");
+    args.AddOption(&order, "-o", "--order", "polynomial order");
+    args.ParseCheck();
 
-//solve problem
+    //generate fine mesh from pgm file
+    PixelMesh fine_mesh(pgm_file);
+
+    //set up problem
+    H1_FECollection fec(order, fine_mesh.GetMesh().Dimension());
+    FiniteElementSpace fine_fes(&fine_mesh.GetMesh(), &fec);
+
+    Array<int> fine_ess_dofs;
+    fine_fes.GetBoundaryTrueDofs(fine_ess_dofs);
+
+    BilinearForm fine_a(&fine_fes);
+    fine_a.AddDomainIntegrator(new DiffusionIntegrator);
+    fine_a.Assemble();
+
+    GridFunction x(&fine_fes);
+    x = 0.0;
+
+    ConstantCoefficient one(1.0);
+    LinearForm b(&fine_fes);
+    b.AddDomainIntegrator(new DomainLFIntegrator(one));
+    b.Assemble();
+
+    SparseMatrix fine_A;
+    Vector X, B;
+    fine_a.FormLinearSystem(fine_ess_dofs, x, b, fine_A, X, B);
+
+    //create multigrid operator
+
+    //solve problem
+
+}
