c4c459c538c812ebfe3e112ffd347e42539a1569
sweetkev <kevinsweet00@gmail.com>
2025-12-26 11:25:44 -0800

    reorganize repo

---
+++ solvers/graph_solver_2d.cpp
@@ -0,0 +1,13 @@
+#include "mfem.hpp"
+#include "ppm.hpp"
+
+using namespace mfem;
+using namespace std;
+
+//generate fine mesh from pgm file
+
+//create finite element space
+
+//create multigrid operator
+
+//solve problem
