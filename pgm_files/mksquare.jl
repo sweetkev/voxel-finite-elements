d78419d45086380f121a723cb54e110a0f77bcc8
Will Pazner <will.e.p@gmail.com>
2026-03-17 15:58:55 -0700

    square mesh

---
+++ pgm_files/mksquare.jl
@@ -0,0 +1,13 @@
+f = open("square.pgm"; write=true)
+
+n = 64
+
+println(f, "P2")
+println(f, n, " ", n)
+println(f, "255")
+for j in 1:n
+    for i in 1:n
+        print(f, "255 ")
+    end
+    println(f)
+end
