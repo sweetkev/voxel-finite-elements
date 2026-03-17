27c914f36573f9c33aa19088b2c040db286f8029
sweetkev <kevinsweet00@gmail.com>
2026-03-17 13:54:43 -0700

    reformat graph_solver.cpp

--- solvers/graph_solver.cpp
+++ solvers/graph_solver.cpp
@@ -58,10 +58,6 @@ int main(int argc, char *argv[])
     Graph fine_graph(fes, image);
     // Create a GraphOperator for the fine graph to get the broken->true dof map
     GraphOperator fine_graph_operator(reference_fes, fine_graph);
-    
-    // Create coarse graph and its corresponding operator
-    Graph coarse_graph = fine_graph.CoarsenGraph();
-    GraphOperator coarse_graph_operator(reference_fes, coarse_graph);
 
     // Create prolongation operator from coarse to fine space
     // Ensure reference element fespace matches the others used
@@ -91,50 +87,6 @@ int main(int argc, char *argv[])
         isotr.SetPointMat(pmats(i));
         fe->GetLocalInterpolation(isotr, local_prolongation(i));
     }
-
-    SparseMatrix *P = new SparseMatrix(coarse_graph_operator.CreateProlongation(
-        local_prolongation,
-        fine_graph,
-        fine_graph_operator.GetBrokenDofToTrueDofMap()));
-    SparseMatrix *coarse_A = new SparseMatrix(coarse_graph_operator.GetMatrix());
-
-    // Create multigrid hierarchy
-    Array<Operator*> operators(2);
-    Array<Solver*> smoothers(2);
-    Array<Operator*> prolongations(1);
-    Array<bool> own_operators(2);
-    Array<bool> own_smoothers(2);
-    Array<bool> own_prolongations(1);
-
-    operators[0] = coarse_A;
-    operators[1] = &A;
-
-    smoothers[0] = new UMFPackSolver(*coarse_A);
-    smoothers[1] = new GSSmoother(A);
-
-    prolongations[0] = P;
-
-    own_operators[0] = true;
-    own_operators[1] = false;
-    own_smoothers = true;
-    own_prolongations = true;
-
-
-    Multigrid mg(operators, smoothers, prolongations, own_operators, 
-                 own_smoothers, own_prolongations);
-
-    //solve problem
-    CGSolver cg;
-    cg.SetRelTol(1e-12);
-    cg.SetMaxIter(2000);
-    cg.SetPrintLevel(1);
-    cg.SetOperator(A);
-    cg.SetPreconditioner(mg);
-    cg.Mult(B, X);
-
-    a.RecoverFEMSolution(X,b,x);
-    x.Save("sol.gf");
-    mesh.GetMesh().Save("mesh.mesh");
 
 //     // If nlevels was not specified, coarsen until height or width is 2
 //     if(nlevels < 0) {
@@ -209,4 +161,53 @@ int main(int argc, char *argv[])
 //     a.RecoverFEMSolution(X,b,x);
 //     x.Save("sol.gf");
 //     mesh.GetMesh().Save("mesh.mesh");
+
+    // Create coarse graph and its corresponding operator
+    Graph coarse_graph = fine_graph.CoarsenGraph();
+    GraphOperator coarse_graph_operator(reference_fes, coarse_graph);
+
+    SparseMatrix *P = new SparseMatrix(coarse_graph_operator.CreateProlongation(
+        local_prolongation,
+        fine_graph,
+        fine_graph_operator.GetBrokenDofToTrueDofMap()));
+    SparseMatrix *coarse_A = new SparseMatrix(coarse_graph_operator.GetMatrix());
+
+    // Create multigrid hierarchy
+    Array<Operator*> operators(2);
+    Array<Solver*> smoothers(2);
+    Array<Operator*> prolongations(1);
+    Array<bool> own_operators(2);
+    Array<bool> own_smoothers(2);
+    Array<bool> own_prolongations(1);
+
+    operators[0] = coarse_A;
+    operators[1] = &A;
+
+    smoothers[0] = new UMFPackSolver(*coarse_A);
+    smoothers[1] = new GSSmoother(A);
+
+    prolongations[0] = P;
+
+    own_operators[0] = true;
+    own_operators[1] = false;
+    own_smoothers = true;
+    own_prolongations = true;
+
+
+    Multigrid mg(operators, smoothers, prolongations, own_operators, 
+                 own_smoothers, own_prolongations);
+
+    //solve problem
+    CGSolver cg;
+    cg.SetRelTol(1e-12);
+    cg.SetMaxIter(2000);
+    cg.SetPrintLevel(1);
+    cg.SetOperator(A);
+    cg.SetPreconditioner(mg);
+    cg.Mult(B, X);
+
+    a.RecoverFEMSolution(X,b,x);
+    x.Save("sol.gf");
+    mesh.GetMesh().Save("mesh.mesh");
+
 }
